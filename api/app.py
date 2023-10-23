7ef8c9f8964d1a3834a08314156eb7e421e5602a
Diveena Nanthakumaran <diveena.nanthakumaran22@imperial.ac.uk>
2023-10-23 15:20:17 +0100

    Adjusted styling according to flask8

--- api/app.py
+++ api/app.py
@@ -2,22 +2,26 @@ from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
 
 @app.route('/images/<path:path>')
 def serve_static(path):
     return send_from_directory('static/images', path)
 
+
 @app.route('/submit', methods=["POST"])
 def submit():
     input_name = request.form.get("name")
     flavor_rating = request.form.get("rating")
-    return render_template("flavor_response.html", 
-                           name=input_name, 
+    return render_template("flavor_response.html",
+                           name=input_name,
                            flavor=flavor_rating,
                            image_link=load_image(flavor_rating))
+
 
 def load_image(flavor_rating):
     if flavor_rating == "Chocolate":
@@ -28,5 +32,5 @@ def load_image(flavor_rating):
         return "images/pistachio.jpg"
     elif flavor_rating == "Strawberry":
         return "images/strawberry.jpg"
-    else: 
+    else:
         return "images/imperial_logo.png"
