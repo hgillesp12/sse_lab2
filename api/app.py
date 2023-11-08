a80f7f88b9c83cf96bd2ac1f12257bd960d30e58
hgg23 <hannah.gillespie23@imperial.ac.uk>
2023-11-08 12:14:55 +0000

    updated example again

--- api/app.py
+++ api/app.py
@@ -21,7 +21,7 @@ def send_to_home():
 def send_to_user():
     return render_template("GitHub.html",
                            message="Enter a GitHub \
-                            username (e.g. ImperialCollegeLondon) and \
+                            username (e.g. real-stanford) and \
                                 we'll display some fun \
                                     information for you.")
 
